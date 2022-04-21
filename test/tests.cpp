75a5838774d8c43611e4d6a0958dd3005377b714
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-04-21 13:39:43 -0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,9 +7,17 @@
 
 TEST_CASE("Ex1", "[example]")
 {
-    int m1[2] = {1,1},
-    m2[2] = {2,2}, m3[2];
-    soma(m1,m2,m3);
+    int *d_v1, *d_v2;
+    int v1[2] = {1,1},
+    v2[2] = {2,2}, v3[2];
+    cudaMalloc(&d_v1, 2*sizeof(int)); 
+    cudaMalloc(&d_v2, 2*sizeof(int));
+    cudaMemcpy(d_v1, v1, 2*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_v2, v2, 2*sizeof(int), cudaMemcpyHostToDevice);
+    soma<<<2,1>>>(2,d_v1,d_v2);
+    cudaMemcpy(v3, d_v2, 2*sizeof(int), cudaMemcpyDeviceToHost);
     for(int i=0;i<2;i++)
-        REQUIRE(m3[i]==m1[i]+m2[i]);
+        REQUIRE(v3[i]==v1[i]+v2[i]);
+    cudaFree(d_v1);
+    cudaFree(d_v2);
 }

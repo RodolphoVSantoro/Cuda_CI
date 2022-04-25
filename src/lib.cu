b0233b9cbc3de73a35e25458b1bfe15c5f668a6f
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-04-25 13:07:55 -0300

    Update lib.cu

--- src/lib.cu
+++ src/lib.cu
@@ -1,8 +1,7 @@
 #include "lib.h"
 #include <stdio.h>
 
-__global__
-void soma(int N, int *v1, int *v2){
+__global__ void soma(int N, int *v1, int *v2){
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   printf("blockid=%d, blockdim=%d, threadid=%d\n", blockIdx.x, blockDim.x, threadIdx.x);
   printf("i=%d, v1=%d, v2=%d\n", i, v1[i], v2[i]);
